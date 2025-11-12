eb0945a562ac3a688701fee3cd55af28bcc952ec
Yuanyuan-Lee <liyuanyuan2024@ia.ac.cn>
2025-11-12 20:32:32 +0800

    commit

--- process_data1.sh
+++
@@ -1,8 +0,0 @@
-python process_data_to_qwen3vl_v1.py \
-    --root-data-dir /share/project/liyuanyuan/code/RoboTwin/data \
-    --save-dir /share/project/liyuanyuan/code/Qwen3-VL/qwen3_vl_data_stack_bowls_4/ \
-    --tasks "stack_bowls_two,stack_bowls_three" \
-    --levels "clean,randomized" \
-    --segment-size 4 \
-    --total-segments 16 \
-    --total-frames 64
